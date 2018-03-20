cfbbf544346ee1ddcb1860d1ac7c47db6dd0647c
laojy1991 <laojy_bupt@163.com>
2018-03-20 15:15:21 +0800

    eurekaServer

---
+++ eurekaServer/src/main/java/com/forezp/EurekaserverApplication.java
@@ -0,0 +1,21 @@
+package com.forezp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaserverApplication implements EmbeddedServletContainerCustomizer{
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+	@Override
+	public void customize(ConfigurableEmbeddedServletContainer container) {
+		container.setPort(8081);
+	}
+}
